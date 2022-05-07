7aadf4dd8f1955a703d153b7321737c0d9823766
wangpf <wangpf@sqbj.com>
2022-05-08 07:18:02 +0800

    init c project

---
+++ data_struct/data_struct/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  data_struct
+//
+//  Created by Arvin on 2022/5/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
